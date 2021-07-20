8f9bb9b37f909dbc8d3afff3763f6520dd12a028
Denis Singh <denis.darkrgd@gmail.com>
2021-07-20 16:12:30 +0300

    Update to task

--- src/App.js
+++ src/App.js
@@ -11,6 +11,20 @@ class App extends Component {
     filter: '',
   };
 
+  componentDidMount() {
+    const contacts = JSON.parse(localStorage.getItem('contacts'));
+
+    if (contacts) {
+      this.setState({ contacts });
+    }
+  }
+
+  componentDidUpdate(prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
+
   handleSubmitContact = data => {
     const { name, number } = data;
 
